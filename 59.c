7804b955d31079c2735e04bd93883dfe6a8d7cc1
Vansh Rathi <ratheevansh583@gmail.com>
2025-10-01 03:18:04 +0530

    Create 59.c

---
+++ 59.c
@@ -0,0 +1,29 @@
+Q59: Count even and odd numbers in an array.
+
+input:
+
+#include<stdio.h>
+int main()
+{
+    int a[100];
+    int n,even=0,odd=0;
+    printf("Enter a limit \n");
+    scanf("%d",&n);
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0; i<n; i++)
+    {
+       if(a[i]%2==0)
+       {
+           even++;
+       }
+       else
+       {
+           odd++;
+       }
+    }
+    printf("EVEN = %d \n",even);
+    printf("ODD = %d",odd);
+}
