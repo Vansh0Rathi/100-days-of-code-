a0ef809829b1a2e2f864ae71cade61f4e49a9750
Vansh Rathi <ratheevansh583@gmail.com>
2025-10-01 03:15:32 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,20 @@
+Q56: Read and print elements of a one-dimensional array.
+
+input:
+
+#include<stdio.h>
+int main()
+{
+    int a[100];
+    int n;
+    printf("Enter a limit \n");
+    scanf("%d",&n);
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0; i<n; i++)
+    {
+        printf("%d ",a[i]);;
+    }
+}
