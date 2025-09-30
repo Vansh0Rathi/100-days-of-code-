a1846ba808218f6fbae79ecc379ff7e66daa671d
Vansh Rathi <ratheevansh583@gmail.com>
2025-10-01 03:12:07 +0530

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,43 @@
+Q52: Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+input:
+
+#include <stdio.h>
+int main()
+{
+    for(int i=1;i<=5;i=i+2)
+    {
+        for(int j=1;j<=i;j++)
+        {
+        printf("*\n");
+        }
+        printf("\n");
+    }
+    for(int i=3;i>=1;i=i-2)
+    {
+        for(int j=1;j<=i;j++)
+        {
+        printf("*\n");
+        }
+        printf("\n");
+    }
+     return 0;
+}
