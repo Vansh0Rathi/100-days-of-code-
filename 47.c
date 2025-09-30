c515372595a71229f226641a5484c0be313d2afb
Vansh Rathi <ratheevansh583@gmail.com>
2025-10-01 03:07:49 +0530

    Create 47.c

---
+++ 47.c
@@ -0,0 +1,25 @@
+Q47: Write a program to print the following pattern:
+*
+**
+***
+****
+*****
+
+input:
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number of rows");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf("* " );
+        }
+        printf("\n");
+    }
+    
+}
