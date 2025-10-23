1b2a0294b02aa5544a7c1f234190d3c436b227df
Vansh Rathi <ratheevansh583@gmail.com>
2025-10-23 22:04:13 +0530

    Update 73.c

--- 73.c
+++ 73.c
@@ -14,38 +14,56 @@ Output 1:
 input:
 
 #include <stdio.h>
-int main (){
-    int x[50][50]={0};
-    int sum[50];
-    int col, row, i,j ;
-    printf ("Enter the amount of rows and column: ");
-    scanf("%d %d",&row,&col);
 
-    for (i=0;i<row;i++)
-    {
-        for (j=0;j<col;j++)
-        scanf("%d",&x[i][j]);
+int main() {
+    int x[50][50] = {0};
+    int sum[50];
+    int col, row, i, j;
+
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &row, &col);
+
+    // Input matrix elements
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < row; i++) {
+        for (j = 0; j < col; j++) {
+            scanf("%d", &x[i][j]);
+        }
     }
-    printf("\n\n");
-    for (i=0;i<row;i++)
-    {
-        for (j=0;j<col;j++)
-        printf("  %d",x[i][j]);
+
+    // Display the matrix
+    printf("\nMatrix is:\n");
+    for (i = 0; i < row; i++) {
+        for (j = 0; j < col; j++) {
+            printf("%4d", x[i][j]);
+        }
         printf("\n");
     }
-printf("\n\n");
-    //addition
-    int sum_;
-    for (i=0;i<row;i++)
-    {
-        sum_=0;
-        for(j=0;j<col;j++)
-        sum_=sum_+x[i][j];
-        sum[i]=sum_;
-    }
-    for(i=0;i<row;i++)
-    {
-        printf("  %d",sum[i]);
+
+    // Calculate sum of each row
+    for (i = 0; i < row; i++) {
+        int sum_ = 0;
+        for (j = 0; j < col; j++) {
+            sum_ += x[i][j];
+        }
+        sum[i] = sum_;
     }
 
+    // Display sum of each row
+    printf("\nSum of each row:\n");
+    for (i = 0; i < row; i++) {
+        printf("Row %d = %d\n", i + 1, sum[i]);
+    }
+
+    // Calculate and display sum of each column
+    printf("\nSum of each column:\n");
+    for (j = 0; j < col; j++) {
+        int col_sum = 0;
+        for (i = 0; i < row; i++) {
+            col_sum += x[i][j];
+        }
+        printf("Column %d = %d\n", j + 1, col_sum);
+    }
+
+    return 0;
 }
